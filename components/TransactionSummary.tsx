31c2e1a9ccc7a0132cab373adad890c3e4c7fa75
GTR1701 <dawidkowal04@gmail.com>
2024-10-03 18:33:48 +0200

    Rounding error in summary fix

--- components/TransactionSummary.tsx
+++ components/TransactionSummary.tsx
@@ -20,11 +20,11 @@ const TransactionSummary = async ({
 			</div>
 			<div className="mx-auto w-1/2 h-32 rounded-2xl p-5 text-xl font-semibold bg-[#282c34] shadow-lg">
 				Aktywa:
-				<p className="pt-5 text-blue-500">{balance} zł</p>
+				<p className="pt-5 text-blue-500">{round(balance, "up", 2)} zł</p>
 			</div>
 			<div className="mx-auto w-1/2 h-32 rounded-2xl p-5 text-xl font-semibold bg-[#282c34] shadow-lg">
 				Przychód:
-				<p className="pt-5 text-green-500">{income} zł</p>
+				<p className="pt-5 text-green-500">{round(income, "up", 2)} zł</p>
 			</div>
 		</div>
 	);
