b955b3809914cee0465f1ef3506e234a6087cdf1
GTR1701 <dawidkowal04@gmail.com>
2025-02-19 13:39:32 +0100

    feat: add monthly balance update function

--- actions/upsertBalance.ts
+++ actions/upsertBalance.ts
@@ -19,9 +19,30 @@ export const upsertBalance = async ({ transaction, type, user }: UpsertBalanceAr
         }
     })
 
+    let thisMonthBalance = await prisma.monthlyBalance.findFirst({
+        where: {
+            userId: user,
+            month: {
+                gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1, 2),
+                lt: new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0, 23, 59, 999)
+            }
+        }
+    })
+
+    console.log(thisMonthBalance)
 
     if (!balance) {
         throw new Error("User not found")
+    }
+
+    if (!thisMonthBalance) {
+        thisMonthBalance = await prisma.monthlyBalance.create({
+            data: {
+                balance: balance.balance,
+                month: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 2),
+                userId: user
+            }
+        })
     }
 
     switch (type) {
@@ -34,6 +55,15 @@ export const upsertBalance = async ({ transaction, type, user }: UpsertBalanceAr
                     balance: balance.balance + transaction
                 }
             })
+
+            await prisma.monthlyBalance.update({
+                where: {
+                    id: thisMonthBalance.id
+                },
+                data: {
+                    balance: thisMonthBalance.balance + transaction
+                }
+            })
             break;
         case 2:
             await prisma.user.update({
@@ -42,6 +72,15 @@ export const upsertBalance = async ({ transaction, type, user }: UpsertBalanceAr
                 },
                 data: {
                     balance: balance.balance - transaction
+                }
+            })
+
+            await prisma.monthlyBalance.update({
+                where: {
+                    id: thisMonthBalance.id
+                },
+                data: {
+                    balance: thisMonthBalance.balance - transaction
                 }
             })
             break;
